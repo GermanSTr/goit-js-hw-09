3b03953cb1ae8d0d267c5871e7bfe5f5a3a3a6b2
GermanSTr <gershev1994@gmail.com>
2023-10-31 21:33:16 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+let isActive = false;
+let intervalColor;
 
+startBtnEl.addEventListener('click', showColor);
+
+function showColor() {
+  if (isActive) {
+    return;
+  }
+  intervalColor = setInterval(() => {
+    const newColor = getRandomHexColor();
+    startBtnEl.parentNode.style.backgroundColor = newColor;
+    isActive = true;
+  }, 1000);
+}
+
+stopBtnEl.addEventListener('click', () => {
+  clearInterval(intervalColor);
+  isActive = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
