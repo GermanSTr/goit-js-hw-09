b4590860272868da27ccdc59f737c34c36006601
GermanSTr <gershev1994@gmail.com>
2023-11-06 00:26:47 +0200

    task-03

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,25 +3,81 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 const inputDateEl = document.querySelector('#datetime-picker');
 const startBtnEl = document.querySelector('button[data-start]');
+const timerEl = document.querySelector('.timer');
+let calendarDate = null;
+let dateNow;
+let isActive = false;
+let intervalId;
+startBtnEl.classList.add('disabled');
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    selectedDate = selectedDates[0];
-    console.log(selectedDate);
+    calendarDate = selectedDates[0];
+    selectedDate();
   },
 };
 
 flatpickr(inputDateEl, options);
-// isPassTime();
 
-// function isPassTime() {
-//   const date = new Date();
-//   if (options < date) {
-//     console.log('1');
-//     window.alert('Please choose a date in the future');
-//   }
-//   console.log('2');
-// }
+function selectedDate() {
+  dateNow = new Date();
+  if (calendarDate < dateNow) {
+    isActive = false;
+    startBtnEl.classList.add('disabled');
+    window.alert('Please choose a date in the future');
+    return;
+  } else {
+    isActive = true;
+    startBtnEl.classList.remove('disabled');
+    startBtn();
+  }
+}
+
+function startBtn() {
+  startBtnEl.addEventListener('click', () => {
+    if (isActive) {
+      startBtnEl.classList.add('disabled');
+      dateNow = new Date();
+      intervalId = setInterval(() => {
+        dateNow = new Date();
+        const diff = calendarDate - dateNow;
+        const time = convertMs(diff);
+        if (calendarDate.getTime() < dateNow.getTime()) {
+          clearInterval(intervalId);
+          return;
+        }
+        render(time);
+      }, 1000);
+    }
+  });
+}
+
+function render({ days, hours, minutes, seconds }) {
+  const lastTime = `${days}:${hours}:${minutes}:${seconds}`;
+  timerEl.textContent = lastTime;
+  timerEl.classList.add('field');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const formEl = document.querySelector('.form');
+const firstDelayEl = formEl.querySelector('input[name="delay"]');
+const delayStepEl = formEl.querySelector('input[name="step"]');
+const amountEl = formEl.querySelector('input[name="amount"]');
+
+formEl.lastElementChild.addEventListener('submit', createPromises);
+
+function createPromises() {
+  const firstDelay = parseInt(firstDelayEl.value);
+  const delayStep = parseInt(delayStepEl.value);
+  const amount = parseInt(amountEl.value);
+
+  for (let i = 0; i < amount; i++) {
+    const promise = createPromise(i, firstDelay + i * delayStep);
+
+    promise
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    const obj = {
+      position,
+      delay,
+    };
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(obj);
+      } else {
+        reject(obj);
+      }
+    }, delay);
+  });
 }
